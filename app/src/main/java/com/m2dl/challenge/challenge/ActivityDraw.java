75235a857f88ac137ac6152c5c7c4652dacb8b1e
rrandy <randy.ratsim@gmail.com>
2015-01-22 10:09:48 +0100

    #2 Menu, faire une photo et l'afficher

---
+++ app/src/main/java/com/m2dl/challenge/challenge/ActivityDraw.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2015 Marine Carrara, Akana Mao, Randy Ratsimbazafy
+ *
+ * This file is part of Tracer c'est gagné.
+ *
+ * Tracer c'est gagné is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Tracer c'est gagné is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tracer c'est gagné.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.m2dl.challenge.challenge;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class ActivityDraw extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_draw);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_activity_draw, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/m2dl/challenge/challenge/ActivityPhoto.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2015 Marine Carrara, Akana Mao, Randy Ratsimbazafy
+ *
+ * This file is part of Tracer c'est gagné.
+ *
+ * Tracer c'est gagné is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Tracer c'est gagné is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tracer c'est gagné.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.m2dl.challenge.challenge;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+//import com.m2dl.miniproject.mini_projetandroid.business.DataStorage;
+
+import java.io.File;
+
+/**
+ * Activité photo
+ * Prise d'une photo
+ */
+public class ActivityPhoto extends ActionBarActivity {
+
+    /**
+     * Adresse uri de l'image
+     */
+    private Uri imageUri;
+    /**
+     * Code pour une capture d'image
+     */
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    /**
+     * Gestion des données de l'application
+     */
+//    private DataStorage storage;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        storage = new DataStorage(this, getResources().getString(R.string.sharedPreferencesFile));
+
+        // Prise de photo
+        takePhoto(null);
+    }
+
+    /**
+     * On veut prendre une photo
+     *
+     * @param view
+     */
+    public void takePhoto(View view) {
+        //Création d'un intent
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+
+        //Création du fichier image
+        File photo = new File(Environment.getExternalStorageDirectory(), "Pic.jpg");
+//        storage.newSharedPreference("photoPath", photo.getAbsolutePath());
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                Uri.fromFile(photo));
+        imageUri = Uri.fromFile(photo);
+
+        //On lance l'intent
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+    }
+
+    //On a reçu le résultat d'une activité
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            //Si l'activité était une prise de photo
+            case CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE:
+                if (resultCode == Activity.RESULT_OK) {
+                    getContentResolver().notifyChange(imageUri, null);
+                }
+        }
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_mini_project, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
